8e6e3c3c7137f234611ce7724a5c6cc9affefea3
Evans Rutto <robbyevans001@gmail.com>
2022-06-04 11:48:08 +0300

    commit tree fce0e166f9e983ed809b7fa42fb9038f105a006f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"];
+
+function  destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name){
+    let newArray=[...cats,name]
+    return newArray;
+}
+appendCat("Broom")
+
+function prependCat(name){
+    let newArray=[name,...cats];
+    return newArray;
+}
+prependCat("Arnold")
+
+function removeLastCat() {
+    let lastCats = cats.slice(0, cats.length - 1);
+    return lastCats;
+}
+function removeFirstCat() {
+    let firstCats = cats.slice(1);
+    return firstCats;
+}
